d49a66ecab96ed34d525aef424687082f0d8e20f
SURYAKANTAP <suryakantab621@gmail.com>
2025-09-12 19:31:40 +0530

    FEAT: Configure SQLAlchemy connection pool for stability

--- app/database.py
+++ app/database.py
@@ -1,4 +1,3 @@
-# app/database.py
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,7 +8,16 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# THE FIX IS HERE: Add connect_args with pool options
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_size=10,  # Number of connections to keep open in the pool
+    max_overflow=2, # Number of extra connections to allow
+    pool_recycle=300, # Recycle connections every 300 seconds (5 minutes)
+    pool_pre_ping=True, # Check connection health before use
+    pool_use_lifo=True, # Use Last-In, First-Out for connection retrieval
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
